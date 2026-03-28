eba76af3c65bfe29ddd5068731c5d70d0caee933
Kakraa3 <clementeklu2004@gmail.com>
2026-03-28 08:41:44 +0000

    Fixed render deploy

--- backend/index.js
+++ backend/index.js
@@ -112,8 +112,10 @@ app.post('/send-newsletter', async (req, res) => {
 
 });
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 const transporter = nodemailer.createTransport({
